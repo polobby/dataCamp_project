e61b483e88435ec999ec908acbcfe9414eaa4f66
JulAgu <jsagudeloac@unal.edu.co>
2024-02-08 23:10:45 +0100

    Change .gitignore

--- submissions/starting_kit/submission_file.py
+++ submissions/starting_kit/submission_file.py
@@ -1,3 +1,5 @@
+import numpy as np
+import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.impute import SimpleImputer
