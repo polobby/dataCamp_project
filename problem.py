2861c16f4182721b735e1c35770aa8dc45213d4f
JulAgu <jsagudeloac@unal.edu.co>
2024-02-08 14:50:33 +0100

    Add in problem.py

---
+++ problem.py
@@ -0,0 +1,7 @@
+import os
+import pandas as pd
+import rampwf as rw
+from sklearn.model_selection import StratifiedShuffleSplit
+
+problem_title = "Energetic Class prediction from housing in Paris"
+
